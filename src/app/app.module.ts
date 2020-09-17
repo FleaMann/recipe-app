5abcd737f374ed1458bbd38ea7bb5a52640cee3e
Felicia Mann <felicia.mann@s02834-mbpr.home>
2020-09-17 13:29:35 -0600

    inital commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './UI/header/header.component';
+import { ShoppingListComponent } from './features/shoppingList/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './features/shoppingList/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './features/recipeBook/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './features/recipeBook/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './features/recipeBook/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule,
